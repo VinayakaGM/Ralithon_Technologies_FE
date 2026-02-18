16d87eb183ba9d978c118537beb4a87f45ce21c2
vishalpanwarit15 <panwarvishal158@gmail.com>
2026-02-18 15:51:34 +0530

    work on search and indexing website

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <head>
+    <meta name="google-site-verification" content="ZLizuXYisr8LiJQG1SuwGheGfyzDba01Jp2LY53XyNk" />
+    </head>
       <body className={inter.className}>
         <SessionProvider>
           <ThemeProvider
