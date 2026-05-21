382c2fe5aed23f23b8ac717fa91fb8e257873876
honeyhh2320 <honeyhh2320@gmail.com>
2026-05-21 12:42:37 +0530

    made changes to date-format in order to handle backend format

--- utils/dateFormat.ts
+++ utils/dateFormat.ts
@@ -1,8 +1,24 @@
 export const formatDate = (
-  date: string | Date | null,
+  date: string | Date | null | undefined,
   includeTime = false
 ) => {
   if (!date) return "N/A";
+
+  let parsedDate: Date;
+
+  // Handle backend format: "06:55:06 21-05-2026"
+  if (typeof date === "string" && date.includes(" ")) {
+    const [time, datePart] = date.split(" ");
+    const [day, month, year] = datePart.split("-");
+
+    parsedDate = new Date(`${year}-${month}-${day}T${time}`);
+  } else {
+    parsedDate = new Date(date);
+  }
+
+  if (isNaN(parsedDate.getTime())) {
+    return "N/A";
+  }
 
   return new Intl.DateTimeFormat("en-IN", {
     day: "2-digit",
@@ -13,5 +29,5 @@ export const formatDate = (
       minute: "2-digit",
       hour12: true,
     }),
-  }).format(new Date(date));
+  }).format(parsedDate);
 };
